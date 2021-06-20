33e16e94bf9c5bebad8be065c6385c1f1a72fca1
Anjali Nandekar <anjali17nandekar@gmail.com>
2021-06-20 23:22:11 +0530

    this is commit from asian time zone

--- src/anju/A.java
+++ src/anju/A.java
@@ -9,17 +9,17 @@ public class A {
 	public static void main(String[] args)
 	{
 		//switch
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\anjal\\Downloads\\chromedriver_win32\\ChromeDriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\anjal\\Downloads\\chromedriver_win32_1\\ChromeDriver.exe");
 		WebDriver obj = new ChromeDriver();
 		obj.get("http://demo.guru99.com/selenium/deprecated.html");
 		obj.switchTo().frame("classFrame");
 		obj.findElement(By.linkText("Deprecated")).click();
 		obj.navigate().back();
-		//obj.switchTo().frame("packageFrame");
-		//obj.findElement(By.linkText("Action")).click();
+		obj.switchTo().frame("packageFrame");
+		obj.findElement(By.linkText("Action")).click();
 		//obj.navigate().back();
-		obj.switchTo().frame("packageListFrame");
-		obj.findElement(By.linkText("com.thoughtworks.selenium")).click();
+		//obj.switchTo().frame("packageListFrame");
+		//obj.findElement(By.linkText("com.thoughtworks.selenium")).click();
 		
 	}
 
